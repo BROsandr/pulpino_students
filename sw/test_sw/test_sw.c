130e7c89b3a0144c745e008b6ffa7c1ecf44c0bf
BROsander <sasha_xarlamov@mail.ru>
2023-04-13 15:48:35 +0000

    commit

--- sw/test_sw/test_sw.c
+++ sw/test_sw/test_sw.c
@@ -8,6 +8,17 @@
 #include <pulpino.h>
 #include <kuznechik.h>
 
+void cipher_and_print( unsigned int data[4] ) {
+  kuznechik_cipher( data );
+  char kuznechik2me[16];
+  kuznechik_get_ciphered( (unsigned int *)kuznechik2me );
+  char buff[17];
+  for( int i = 0; i < 16; ++i ) {
+    buff[15-i] = kuznechik2me[i];
+  }
+  buff[16] = '\0';
+  uart_send( buff, 16 );
+}
 
 int main()
 {
@@ -25,29 +36,16 @@ int main()
   // set_pin_function(1, FUNC_GPIO);
   // set_gpio_pin_direction(1, DIR_IN);
 
-  set_pin_function(31, FUNC_GPIO);
-  set_gpio_pin_direction(31, DIR_OUT);
+  unsigned int me2kuznechik[4];
+  me2kuznechik[3] = 0x3ee5c99f;
+  me2kuznechik[2] = 0x9a41c389;
+  me2kuznechik[1] = 0xac17b4fe;
+  me2kuznechik[0] = 0x99c72ae4;
+  cipher_and_print( me2kuznechik );
 
-
-  while(1) {
-    // char mode = get_gpio_pin_value(1);
-    unsigned int me2kuznechik[4];
-    me2kuznechik[0] = 0x3ee5c99f;
-    me2kuznechik[1] = 0x9a41c389;
-    me2kuznechik[2] = 0xac17b4fe;
-    me2kuznechik[3] = 0x99c72ae4;
-
-    kuznechik_cipher( me2kuznechik );
-    char kuznechik2me[16];
-    kuznechik_get_ciphered( (unsigned int *)kuznechik2me );
-    // char *reg_addr = 0x00008200;
-    // for( int i = 0; i < 16; ++i ) {
-    //   *reg_addr = kuznechik2me[i];
-    // }
-    char buff[17];
-    memcpy( buff, kuznechik2me, 16 );
-    buff[16] = '\0';
-    set_gpio_pin_value(31, 1);
-    uart_send( buff, 17 );
-  }
+  me2kuznechik[3] = 0x79cfed3c;
+  me2kuznechik[2] = 0x39fa7677;
+  me2kuznechik[1] = 0xb970bb42;
+  me2kuznechik[0] = 0xa5631ccd;
+  cipher_and_print( me2kuznechik );
 }
