1d615ab873ef3454ad395f30165acba61bd891ca
alexkharl <sasha_xarlamov@mail.ru>
2023-05-19 15:54:54 +0300

    Feat(pos_wrap):Исправление вычисления позиций

--- rtl/mouse_pos_wrapper.sv
+++ rtl/mouse_pos_wrapper.sv
@@ -53,8 +53,20 @@ module mouse_pos_wrapper #(
       y_pos_ff <= y_pos_next;
     end 
 
-  assign y_pos_next = y_pos_ff + { { (X_POS_WIDTH-9){xm[8]} }, xm };
-  assign x_pos_next = x_pos_ff - { { (Y_POS_WIDTH-9){ym[8]} }, ym };
+  // assign y_pos_next = y_pos_ff + { { (X_POS_WIDTH-9){xm[8]} }, xm };
+  // assign x_pos_next = x_pos_ff - { { (Y_POS_WIDTH-9){ym[8]} }, ym };
+
+  
+  wire        sx = xm[8];		// signs
+  wire        sy = ym[8];		
+  wire [8:0]  ndx = sx ? {0,~xm[7:0]}+1 : {0,xm[7:0]};	// magnitudes
+  wire [8:0]  ndy = sy ? {0,~ym[7:0]}+1 : {0,ym[7:0]};
+   
+	assign y_pos_next = sx ? (y_pos_ff>ndx ? y_pos_ff - ndx : 0) 
+			             : (y_pos_ff < HEIGHT - ndx ? y_pos_ff+ndx : HEIGHT);
+  // note Y is flipped for video cursor use of mouse
+  assign x_pos_next = sy ? (x_pos_ff < WIDTH - ndy ? x_pos_ff+ndy : WIDTH)
+			  : (x_pos_ff>ndy ? x_pos_ff - ndy : 0);
 
   always_ff @( posedge clk_i or negedge rstn_i )
     if( ~rstn_i )  btnm_ff <= '0;
