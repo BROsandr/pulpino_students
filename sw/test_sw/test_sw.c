b3ef55589f4072917d87bcd3e4d338ed35f5a161
alexkharl <sasha_xarlamov@mail.ru>
2023-11-18 19:20:31 +0300

    Feat(test_sw):Программа для проверки всего vga

--- sw/test_sw/test_sw.c
+++ sw/test_sw/test_sw.c
@@ -17,9 +17,16 @@ int main()
 
   set_gpio_pin_value(31, 1);
 
-  uint8_t* vga_buff = (uint8_t*)VGA_BASE_ADDR;
+  uint32_t* vga_buff = (uint32_t*)VGA_BASE_ADDR;
 
-  *vga_buff = 0x41;
+  // Fill the screen with 0. Mark the end with 1.
+  for (int i = 0; i < 60; ++i) {
+    for (int j = 0; j < 80; j += 4) {
+      if (i == 59 && j == 76) *vga_buff = (0x30 << 24 ) | (0x30 << 16 ) | (0x30 << 8 ) | 0x31;
+      else                    *vga_buff = (0x30 << 24 ) | (0x30 << 16 ) | (0x30 << 8 ) | 0x30;
+      ++vga_buff;
+    }
+  }
 
   while(1) {
     asm volatile ("nop");
